47ff9c280c8c81aad37b8fd14828790dede2b730
Jorge Espinoza <jorgeespinozaortega@gmail.com>
2021-02-01 19:38:38 -0300

    Add buttons and spinner
    List items on nav are buttons now, and a spinner appears when the page
    is loading.

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 $(document).ready(function(){
 
   $('.nav-item').click(function(){
-    $('.nav-item').removeClass('font-weight-bold');
-    $(this).addClass('font-weight-bold');
+    $('.nav-item').removeClass('btn-primary');
+    $(this).addClass('btn-primary');
     $('.card').addClass('d-none');
     $('.card:contains(' + $(this).text() + ')').removeClass('d-none');
   })
+
+  $('.card').addClass('text-left');
+  $('.spinner-border').hide();
 
 });
